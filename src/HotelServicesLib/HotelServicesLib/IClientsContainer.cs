619b28b4c5301865e88bdd3c4d68ca1433c20534
DiKey <dapetrin98@yandex.ru>
2019-02-10 18:51:26 +0300

    Добавлен интерфейс контейнера клиентов

---
+++ src/HotelServicesLib/HotelServicesLib/IClientsContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelServicesLib
+{
+    public interface IClientsContainer
+    {
+        void AddClient(Client client);
+        void RemoveClient(Client client);
+        Client GetClientById(string id);
+    }
+}
